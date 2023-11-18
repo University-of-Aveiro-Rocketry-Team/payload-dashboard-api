3392fcc0361f4085c3f315256bed29af01bc2c2d
André Clérigo <andreclerigo@outlook.com>
2023-11-18 13:57:53 +0000

    changing nc host

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -5,13 +5,19 @@ docker compose down
 docker compose up -d
 
 # Netcat check with 10-minutes timeout
-timeout=600 # 10 minutes in seconds
-while ! nc -z app 3000 && [ $timeout -gt 0 ]; do
+timeout=600  # 10 minutes
+port=3000    # main app service port
+
+until nc -z "$host" $port || [ $timeout -eq 0 ]; do
+  >&2 echo "Service is unavailable - sleeping"
   sleep 5
-  timeout=$((timeout - 5))
+  timeout=$((timeout-5))
 done
 
-if [ $timeout -le 0 ]; then
-  echo "Application failed after 10 minutes"
+if [ $timeout -eq 0 ]; then
+  >&2 echo "App did not become available after 10 minutes"
   exit 1
 fi
+
+>&2 echo "App is up"
+
